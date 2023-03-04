3c55d9d8d8a608328c97eb94a08820114594e151
sonu kumar <sonukumar7505891@gmail.com>
2023-03-05 02:11:41 +0530

    create a selectionsorting program

---
+++ SelectionSort.java
@@ -0,0 +1,20 @@
+class SelectionSort{
+    public static void main(String [] args){
+        int [] a={36,1,2,12,5};
+        int min,temp=0;
+        for(int i=0;i<a.length;i++){
+            min=i;
+            for(int j=i+1;j<a.length;j++){
+                if(a[j]<a[min]){
+                   min=j;
+                }
+            }
+                    temp=a[i];
+                    a[i]=a[min];
+                    a[min]=temp;
+        }
+        for(int i=0;i<a.length;i++){
+                System.out.print(" "+a[i]);
+        }
+    }
+}
