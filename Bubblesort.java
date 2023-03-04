ee65b9a4f9118de1f0d188cc4e11d5c180199abc
sonu kumar <sonukumar7505891@gmail.com>
2023-03-05 02:36:46 +0530

    add Bubblesort program

---
+++ Bubblesort.java
@@ -0,0 +1,23 @@
+class Bubblesort{
+    public static void main(String [] args){
+        int [] a={36,1,2,12,5};
+        int temp;
+        for(int i=0;i<a.length;i++){
+            int flag=0;
+            for(int j=0;j<a.length-1-i;j++){
+                if(a[j]>a[j+1]){
+                    temp=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+                    flag=1;
+                }
+            }
+            if(flag==0){
+                break;
+            }
+        }
+        for(int i=0;i<a.length;i++){
+                System.out.print(" "+a[i]);
+        }
+    }
+}
